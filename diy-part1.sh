9de3cb84deba0c4abadc15183b48b7cfc5f7bc1c
Mattaclp <51333346+Mattaclp@users.noreply.github.com>
2020-12-14 11:22:35 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,5 +13,11 @@
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
+# add qBittorrent
+svn co https://github.com/kwokwai6618/lede/trunk/package/lean/qBittorrent package/lean/qBittorrent
+svn co https://github.com/kwokwai6618/lede/trunk/package/lean/rblibtorrent package/lean/rblibtorrent
+svn co https://github.com/kwokwai6618/lede/trunk/package/lean/luci-app-qbittorrent
+rm -rf package/lean/luci-app-qbittorrent && mv luci-app-qbittorrent package/lean/
+
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
